da75b0e7d612fa73d1f129cf67c5b65caf0284ee
Luis Benitez <lfbenitez15@gmail.com>
2021-06-07 01:28:11 +0000

    Se corrigio errores de escritura en el commit anterior Aun pendiente la asignacion de permisos

--- __manifest__.py
+++ __manifest__.py
@@ -24,8 +24,7 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',	    
+        'views/views.xml',	    
         'views/Course.xml',
     ],
     # only loaded in demonstration mode
